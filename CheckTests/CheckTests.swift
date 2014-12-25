8d318239fa79d93e4fa177ace6da667b628793b1
Austen Allred <austen.allred@gmail.com>
2014-12-25 01:29:02 -0700

    Initial Commit

---
+++ CheckTests/CheckTests.swift
@@ -0,0 +1,36 @@
+//
+//  CheckTests.swift
+//  CheckTests
+//
+//  Created by Austen Allred on 12/25/14.
+//  Copyright (c) 2014 Austen Allred. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CheckTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
